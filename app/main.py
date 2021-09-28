1e555a0b62e40402733278583e39073800fa17f1
chemuske013 <salim.benahmed@supcom.tn>
2021-09-28 08:59:14 +0100

    app working, no database yet

--- app/main.py
+++ app/main.py
@@ -21,10 +21,28 @@ def root(request: Request):
     return templates.TemplateResponse("index.html", context={'request': request})
 
 @app.post("/")
-def check(request: Request):
+def check(request: Request,
+    first_name: str = Form(...),
+    last_name: str = Form(...),
+    age: int = Form(...),
+    gender: int = Form(...),
+    pregnancies: int = Form(...), 
+    glucose: float = Form(...),
+    blood_pressure: float = Form(...),
+    skin_thickness: float = Form(...),
+    insulin: float = Form(...),
+    height: float = Form(...),
+    weight: float = Form(...),
+    dpf: float = Form(...)
+    ):
     
     #({"first_name": first_name, "last_name": last_name, "age": age, "gender": gender})
-    x = [[0, 1, 1, 1, 1, 1, 1, 1]]
-    result = model.predict(x)
-
+    
+    bmi = weight/((height/100)**2)
+    x = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]]
+    prediction = model.predict(x)
+    #return [first_name, last_name, gender, x]
+    result = False
+    if( prediction ) :
+        result = True
     return templates.TemplateResponse("result.html", context={'request': request, 'result': result})
