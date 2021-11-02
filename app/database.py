25a193ea397bc98ea7166ce4a1a8799948b476de
chemuske013 <68511701+chemuske013@users.noreply.github.com>
2021-11-02 19:06:08 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://sql11445626:mKuNyjAdcx@sql11.freemysqlhosting.net:3306/sql11445626"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://sql11448537:uUGpSfRn2p@sql11.freemysqlhosting.net:3306/sql11448537"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
